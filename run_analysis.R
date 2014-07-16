dff00a9edb3a89bc1858e78e258f16d16bea7feb
Jim Thompson <jimthompson5802@aol.com>
2014-07-15 20:57:54 -0400

    modified variable names & added code to create second data set

--- run_analysis.R
+++ run_analysis.R
@@ -9,18 +9,28 @@ unzip("./getdata_projectfiles_UCI_HAR_Dataset.zip")
 # read in activity labels
 #
 activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
-names(activity) <- c("Activity.Code","Activity.Label")
+names(activity) <- c("activity.code","activity.label")
 
 #
 # create feature names
 #
 fname.df <- read.table("./UCI HAR Dataset/features.txt")
+
+# show sample of original feature names
 head(fname.df)
 tail(fname.df)
 
-# make the feature names legal for R
+#
+# make the feature names legal for R 
+# using Google's R style guide as basis for naming standard 
+# https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml#identifiers
+#
 features <- gsub("\\.\\.\\.","\\.",make.names(fname.df[,2]))
-features <- sub("\\.\\.$","",features)
+features <- gsub("\\.\\.$","",features)
+features <- gsub("\\.$","",features)
+features <- tolower(features)
+
+# show sample of feature names after standardization
 head(features)
 tail(features)
 
@@ -31,15 +41,16 @@ train.X <- read.table("./UCI HAR Dataset/train/X_train.txt")
 names(train.X) <- features
 
 train.y <- read.table("./UCI HAR Dataset/train/y_train.txt")
-names(train.y) <- "Activity.Code"
+names(train.y) <- "activity.code"
 train.y <- merge(train.y,activity)
 
 train.id <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-names(train.id) <- "Subject.Id"
+names(train.id) <- "subject.id"
 
 # combined parts of the training data
 
-train.df <- cbind(Data.Source=rep("train",nrow(train.id)),train.id,train.y,train.X)
+train.df <- cbind(data.source=rep("train", nrow(train.id)), 
+                  train.id, train.y, train.X)
 
 
 #
@@ -49,38 +60,55 @@ test.X <- read.table("./UCI HAR Dataset/test/X_test.txt")
 names(test.X) <- features
 
 test.y <- read.table("./UCI HAR Dataset/test/y_test.txt")
-names(test.y) <- "Activity.Code"
+names(test.y) <- "activity.code"
 test.y <- merge(test.y,activity)
 
 test.id <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-names(test.id) <- "Subject.Id"
+names(test.id) <- "subject.id"
 
 #combine parts of the test 
-test.df <- cbind(Data.Source=rep("test",nrow(test.id)),test.id,test.y,test.X)
+test.df <- cbind(data.source=rep("test", nrow(test.id)), 
+                 test.id, test.y, test.X)
 
 #
 #combine train and test sets
 #
-all.df <- rbind(train.df,test.df)
-head(all.df[,1:6])
-tail(all.df[,1:6])
+all.df <- rbind(train.df, test.df)
+
+# show sample of data
+head(all.df[, 1:6])
+tail(all.df[, 1:6])
 
 #
 # create first data set with just mean and standard deviation measurements
 #
 
-#get index for features with mean or standard deviaion measures
-mean.idx <- grepl("mean",names(all.df),ignore.case=TRUE)
-std.idx <- grepl("std",names(all.df),ignore.case=TRUE)
+#get column index for feature names containing mean or standard deviaion measures
+mean.idx <- grepl("mean", names(all.df), ignore.case=TRUE)  # features with mean
+std.idx <- grepl("std", names(all.df), ignore.case=TRUE)  # features with standard deviaion 
 
-# extract out identifier attributes and mean and standard deviation
-data.set.one <- cbind(all.df[,1:4],all.df[,mean.idx | std.idx])
+# extract out attributes for mean and standard deviation
+data.set.one <- cbind(all.df[, 1:4], all.df[, mean.idx | std.idx])
 
-#resulted attributes for data.set.one
+# resulted attributes for data.set.one
 names(data.set.one)
 
 #
-# create second data set
+# create second tidy data set
 #
+library(plyr)
+# function to calculate means of all columns except those used for idendification
+# assumes the identifier columns are the first 4 columns
+# used in conjunction with ddply() function.
+CalcMeans <- function(df) {
+    colMeans(df[, 5:ncol(df)])
+}
+
+# calculate mean of each numeric feature by subject and activity label
+data.set.two <- ddply(data.set.one, .(subject.id,activity.label), CalcMeans)
 
 
+#
+# Write out second tidy data set
+#
+write.csv(data.set.two, "./accelerometer_summary_data.csv",row.names=FALSE)
